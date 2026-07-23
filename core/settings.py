9501dc961527082f8353ea687ada2b6b3ebd7216
cj-tech517 <cdiamouangana@byupathway.edu>
2026-07-23 17:43:47 +0200

    do

--- core/settings.py
+++ core/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1+74v4rp(m40b6q#xz%1psgf4zudol)^-i2cn2092k-s=6u08_'
-
+SECRET_KEY = '7b4c789d271d71defd77eb035d852638'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
